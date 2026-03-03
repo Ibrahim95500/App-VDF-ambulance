2c2ce5c8ef01478ab7123b7eaa6815f7387d60d1
ibrahim nifa <ibrahimnifa@mbp-de-ibrahim.home>
2026-03-03 01:25:53 +0100

    fix(seed): instantiate PrismaClient with adapter options to resolve initialization error outside edge context

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,12 @@
 import { PrismaClient, Role } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 import bcrypt from 'bcryptjs'
 
-const prisma = new PrismaClient()
+const connectionString = process.env.DATABASE_URL
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
 
 async function main() {
     const rhEmail = process.env.RH_ADMIN_EMAIL || 'admin@ambulance.com'
