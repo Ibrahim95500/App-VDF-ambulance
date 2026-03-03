e102eba935a3d5218eed881491a0ff2b5753ecd5
ibrahim nifa <ibrahimnifa@mbp-de-ibrahim.home>
2026-03-03 14:57:08 +0100

    fix(prisma): ignore dotenv in production

--- prisma.config.js
+++ prisma.config.js
@@ -1,4 +1,8 @@
-require('dotenv/config');
+try {
+    require('dotenv/config');
+} catch (e) {
+    // Ignore in production where dotenv might not be installed
+}
 
 /** @type {import('@prisma/config').PrismaConfig} */
 module.exports = {
