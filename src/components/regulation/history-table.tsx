9bdda0e8e012c02bd5713e2aa542ba108215022e
ibrahim nifa <ibrahimnifa@mbp-de-ibrahim.home>
2026-03-14 02:43:31 +0100

    fix: add missing cn import in history table

--- src/components/regulation/history-table.tsx
+++ src/components/regulation/history-table.tsx
@@ -22,6 +22,7 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 import { HistoryDetailsDialog } from "./history-details-dialog"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { Calendar } from "@/components/ui/calendar"
+import { cn } from "@/lib/utils"
 
 interface HistoryTableProps {
     data: any[]
