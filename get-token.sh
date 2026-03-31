e9a4b0ed398f0b5dc455e86f75eae032ac79906d
ibrahim nifa <ibrahimnifa@macbookpro.home>
2026-03-31 13:27:22 +0200

    Fix: Nettoyage des scripts de tests locaux provoquant l'échec du build

--- get-token.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Veuillez coller ce code dans Hostinger pour récupérer le vrai token :"
-echo "docker exec vdf-db-dev sh -c 'psql -U \$POSTGRES_USER -d \$POSTGRES_DB -c \"SELECT token FROM \\\"FcmToken\\\" LIMIT 1;\"'"
